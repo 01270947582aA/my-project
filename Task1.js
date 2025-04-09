523a560d3359261b06699b331b4933a34fbdc5fd
01270947582aA <youssefsadiq66@gmail.com>
2025-04-09 12:27:02 +0200

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,27 @@
+let student = [1, 4, 6, "ahmed", "omar"];
+let i = 0;
+while (i < student.length) {
+  if (typeof student[i] === "string") {
+    console.log(student[i]);
+  }
+  i++;
+}
+
+let employe = [1, 4, 6, "ahmed", "omar"];
+for (let i = 0; i < employe.length; i++) {
+  if (typeof employe[i] === "string") {
+    console.log(employe[i]);
+  }
+}
+
+
+// var frinds=new Array("Ahmed", "soha", "omar");
+// console.log(frinds);
+
+
+// var frinds=new Array();
+// frinds[0]=Ahmed;
+// frinds[1]=yousef;
+// frinds[2]=omar;
+// frinds[3]=Ali;
+// console.log(frinds);
